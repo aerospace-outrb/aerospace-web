add57d9b22132e9011581f15467615a40219e202
Sanjay <59828852+sanjay-swain@users.noreply.github.com>
2023-04-12 13:22:51 +0530

    Create Contact Page

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('about/', views.about, name="about"),
     path('members/', views.members, name="members"),
-    path('gallery/', views.gallery, name="gallery")
+    path('gallery/', views.gallery, name="gallery"),
+    path('contact/', views.contact, name="contact")
 ]
--- main/views.py
+++ main/views.py
@@ -15,3 +15,7 @@ def members(request):
 
 def gallery(request):
     return render(request, 'main/gallery.html')
+
+
+def contact(request):
+    return render(request, 'main/contact.html')
